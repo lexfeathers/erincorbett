cb4f7d9c133977a2aaf0d65761fe47531b0cf38f
Lex Feathers <lex@gameaudio.ca>
2025-08-19 17:25:18 -0400

    Sorting portfolio collection by date

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,23 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 
 // 2. Import loader(s)
-import { glob, file } from 'astro/loaders';
+import { glob } from 'astro/loaders';
 
 // 3. Define your collection(s)
 const portfolio = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/pages/projects" }),
+    loader: glob({ pattern: "*.md", base: "./src/pages/projects" }),
     schema: z.object({
+        // url: z.string().url(),
         title: z.string(),
-        description: z.string(),
+        client: z.string(),
+        thumbnail: z.string(),
+        image: z.string(),
+        image_description: z.string(),
+        status: z.string(),
         pubDate: z.coerce.date(),
+        description: z.string(),
+        workType: z.array(z.string()),
     })
 });
 
